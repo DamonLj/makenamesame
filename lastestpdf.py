79a4216e3d5589f7c8006a508c46afacad2f8de7
DamonLj <ljdamon@163.com>
2020-07-15 00:46:15 +0800

    添加删除旧版pdf功能

--- lastestpdf.py
+++ lastestpdf.py
@@ -4,21 +4,42 @@
 __author__ = 'Damon'
 
 import os
+import pandas as pd
 
 
 class PdfFile():
-    def __init__(self, filepath):
-        self.fp = filepath
+    def __init__(self, filename_ex):
+        self.fn_ex = filename_ex
+        self.fn, self.ex = os.path.splitext(self.fn_ex)
+        self.n = self.get_num()
+        self.v = self.get_vision()
 
     def get_num(self):
-        return self.n
+        n = self.fn.split("_")[0]
+        return n
 
     def get_vision(self):
-        return self.v
+        if self.fn[-1] == "D":
+            v = 0
+        elif self.fn[-1] == ")":
+            v = self.fn[-2]
+        else: pass
+        return v
 
 class LastestPdf():
     def __init__(self, dir):
         self.d = dir
+        self.list = []
 
     def lastest_pdf(self):
-        pass
+        for f in os.listdir(self.d):
+            f_c = PdfFile(f)
+            self.list.append((f, f_c.n, int(f_c.v)))
+        df = pd.DataFrame(self.list, columns=["filename", "number", "virsion"])
+        # g1 = df.groupby("number").apply(lambda t:t[t.virsion==t.virsion.max()])
+        idx = df.groupby("number")["virsion"].idxmax()
+        print(idx)
+        return
+
+dir = "D:\Downloads\pdf"
+LastestPdf(dir).lastest_pdf()
