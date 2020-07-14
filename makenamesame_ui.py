e4f86a0504c1609c60651f294179081a1f76ce0b
IPPR-LJM <ljmgps@163.com>
2020-07-14 17:21:16 +0800

    优化注释和命名

--- makenamesame_ui.py
+++ makenamesame_ui.py
@@ -12,32 +12,33 @@ from tkinter import messagebox as mb
 
 class MnsUI(tk.Frame):
     def __init__(self, master=None):
-        super().__init__(master)
+        super().__init__(master)  #初始化此框架的主窗口类
         self.master = master
-        self.pack()
+        self.pack()  #放置框架在窗口上
         self.create_widgets()
         self.master.title("中元北京数审图纸命名工具")
-        self.master.update()
+        self.master.update()  #更新获取窗口大小，用于窗口居中
+        # 获取屏幕分辨率，算出窗口左上角坐标定位
         self.x_ = self.master.winfo_screenwidth() / 2 - self.master.winfo_width() / 2
         self.y_ = self.master.winfo_screenheight() / 2 - self.master.winfo_height() / 2
         self.master.geometry("+%d+%d" % (self.x_, self.y_))
 
     def create_widgets(self):
-        l1 = tk.Label(self, text="文件夹路径:", font=("微软雅黑", 12))
-        l1.grid(row=1, column=0, padx=20)
+        l_1 = tk.Label(self, text="文件夹路径:", font=("微软雅黑", 12))
+        l_1.grid(row=1, column=0, padx=20)
 
         self.dirpath = tk.StringVar()
-        dirpath_entry = tk.Entry(self, textvariable=self.dirpath, font=("微软雅黑", 12))
-        dirpath_entry.grid(row=1, column=1, ipadx=150)
+        e_dirpath = tk.Entry(self, textvariable=self.dirpath, font=("微软雅黑", 12))
+        e_dirpath.grid(row=1, column=1, ipadx=150)
 
-        getdir_button = tk.Button(self, text="选择文件夹", command=self.get_dirpath, font=("微软雅黑", 12))
-        getdir_button.grid(row=1, column=2, padx=20)
+        b_getdir = tk.Button(self, text="选择文件夹", command=self.get_dirpath, font=("微软雅黑", 12))
+        b_getdir.grid(row=1, column=2, padx=20)
 
-        makenamesame_button = tk.Button(self, text="重命名文件", command=self.make_allname_same, font=("微软雅黑", 12))
-        makenamesame_button.grid(row=2, column=1, pady=20)
+        b_makenamesame = tk.Button(self, text="重命名文件", command=self.make_allname_same, font=("微软雅黑", 12))
+        b_makenamesame.grid(row=2, column=1, pady=20)
 
-        explanation = tk.Label(self)
-        explanation["text"] ='''
+        l_explanation = tk.Label(self)
+        l_explanation["text"] ='''
 *********************************************说  明************************************************************
         本程序用于中元报审北京数字审图项目，批量将PDF文件名更改为与PDF图号相同的dwf的文件名。
         1、dwf和pdf放在同一文件夹下。
@@ -51,8 +52,8 @@ class MnsUI(tk.Frame):
         U001G1-A0020-1202_二层平面图.dwf        U001G1-A0020-1202_二层平面图.dwf
 ********************************by damon************************ljmgps**********************************
         '''
-        explanation["justify"] = tk.LEFT
-        explanation.grid(row=0, column=1)
+        l_explanation["justify"] = tk.LEFT  #设置文字左对齐
+        l_explanation.grid(row=0, column=1)
 
     def get_dirpath(self):
         self.path_ = fd.askdirectory()
@@ -62,12 +63,12 @@ class MnsUI(tk.Frame):
         dir = self.dirpath.get()
         result = Makenamesame().make_alldir_same(self.dirpath.get())
         if result:
-            mb.showinfo(title="Info", message="重命名成功！")
+            mb.showinfo(title="Info", message="重命名成功！")  #使用message类弹出新窗口
         else:
             mb.showerror(title="Info", message="文件夹路径错误！")
 
     def cancel(self):
-        self.master.destroy()
+        self.master.destroy()  #关闭窗口，不是框架，所以是master
 
 
 class Makenamesame():
